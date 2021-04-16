941ef7b3e3d5e50f7a4ee7226030a4e0984d44f9
c-my <cmy1113@outlook.com>
2021-04-16 19:58:37 +0800

    feat: fetch page url in list page

--- main.py
+++ main.py
@@ -3,13 +3,39 @@ import json
 
 from spider import WikiSpider
 
-keywords = ["AK-47突击步枪", "CAESAR自行火炮", "东北大学_(中国)", "北京理工大学"]
+Military_list_of_lists_url_list = [
+    "https://en.wikipedia.org/wiki/Lists_of_accidents_and_incidents_involving_military_aircraft",
+    "https://en.wikipedia.org/wiki/Lists_of_armoured_fighting_vehicles",
+    "https://en.wikipedia.org/wiki/List_of_artillery", "https://en.wikipedia.org/wiki/Lists_of_gun_cartridges",
+    "https://en.wikipedia.org/wiki/Lists_of_military_aircraft_by_nation",
+    "https://en.wikipedia.org/wiki/Lists_of_Bulgarian_military_aircraft",
+    "https://en.wikipedia.org/wiki/Lists_of_military_equipment",
+    "https://en.wikipedia.org/wiki/Lists_of_currently_active_military_equipment_by_country",
+    "https://en.wikipedia.org/wiki/Lists_of_military_installations",
+    "https://en.wikipedia.org/wiki/Lists_of_naval_flags", "https://en.wikipedia.org/wiki/Lists_of_swords",
+    "https://en.wikipedia.org/wiki/Lists_of_weapons",
+    "https://en.wikipedia.org/wiki/Lists_of_World_War_II_military_equipment",
+]
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read("config.ini")
     s = WikiSpider(config)
+    link_set = set()
+    try:
+        for list_url in Military_list_of_lists_url_list:
+            list_list = s.get_lists(list_url)
+            for list_page in list_list:
+                new_url = s.get_links_from_list(list_page)
 
-    with open("result.json", 'w', encoding="utf-8") as f:
-        result_list = [s.search_by_chinese(k) for k in keywords]
-        f.write(json.dumps(result_list, ensure_ascii=False))
+                link_set |= new_url
+    except Exception as e:
+        with open("urls.txt", "w", encoding="utf-8") as f:
+            for u in link_set:
+                f.write(u + "\n")
+        print(e)
+    else:
+        with open("urls.txt", "w", encoding="utf-8") as f:
+            for u in link_set:
+                f.write(u + "\n")
+    print(len(link_set))
--- spider.py
+++ spider.py
@@ -8,7 +8,7 @@ from pandas.io.html import read_html
 class WikiSpider:
     def __init__(self, config):
         self.proxy_config = config["PROXY"]
-        self.wiki_base_url = "https://zh.wikipedia.org"
+        self.wiki_base_url = "https://en.wikipedia.org"
         self.wiki_url = self.wiki_base_url + "/wiki/"
 
     def search_by_chinese(self, keyword):
@@ -58,8 +58,44 @@ class WikiSpider:
             img_urls.append(img_url)
         return img_urls
 
-    def get_wiki_url(self, keyword) -> str:
+    def get_lists(self, lists_of_lists_url: str) -> set[str]:
+        """
+        get lists from a wikipedia lists_of_lists page
+        :param lists_of_lists_url: page url
+        :return: set of lists
+        """
+        r = requests.get(lists_of_lists_url, proxies=self.get_proxy())
+        body = r.text
+        soup = BeautifulSoup(body, features="lxml")
+        links = soup.findAll("a", {"title": re.compile(r"^List of[\s\S]*$")})
+        list_link_set = {self.wiki_base_url + link["href"] for link in links}
+        return list_link_set
+
+    def get_links_from_list(self, list_url: str) -> set[str]:
+        """
+        get all related links in wikipedia list page
+        :param list_url: page url
+        :return: set of related links
+        """
+        r = requests.get(list_url, proxies=self.get_proxy())
+        body = r.text
+        soup = BeautifulSoup(body, features="lxml")
+        possible_area = soup.findAll("div", {"class": "mw-parser-output"})
+        if len(possible_area) == 0:
+            return set()
+        area = possible_area[0]
+
+        links = area.findAll("a", {"href": re.compile(r"^(/wiki/)[\s\S]*$")})
+        link_set = {self.wiki_base_url + link["href"] for link in links if self.is_content_page(link["href"])}
+        return link_set
+
+    def get_wiki_url(self, keyword: str) -> str:
         return self.wiki_url + keyword
+
+    def is_content_page(self, url: str) -> bool:
+        return not (":" in url or
+                    url.startswith("/wiki/List_of") or
+                    url.startswith("/wiki/Lists_of"))
 
     def get_proxy(self) -> dict:
         return {
