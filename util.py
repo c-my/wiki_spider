db61f9f3c4902efa78967a986cdece18e7a016f3
c-my <cmy1113@outlook.com>
2021-06-06 20:09:33 +0800

    feat: baidubaike utils

--- util.py
+++ util.py
@@ -1,6 +1,22 @@
+import os
+
 url_file = "data/en_wiki_urls.txt"
 
-if __name__ == '__main__':
+
+def build_baidubaike_url_list():
+    file_list = []
+    files = os.listdir("data/baidubaike")
+    for file in files:
+        file_list.append(file)
+    with open("data/baidu_baike_urls.txt", "w", encoding="utf-8") as f:
+        for file in file_list:
+            with open("data/baidubaike/" + file, "r", encoding="utf-8") as g:
+                urls = g.readlines()
+            for url in urls:
+                f.write("https://baike.baidu.com/item/" + url)
+
+
+def build_wiki_url_list():
     line_list = []
     with open(url_file, "r", encoding="utf-8") as f:
         line_list = f.readlines()
@@ -12,3 +28,7 @@ if __name__ == '__main__':
     with open("t.txt", "w", encoding="utf-8") as f:
         for r in result:
             f.write(r)
+
+
+if __name__ == '__main__':
+    build_baidubaike_url_list()
